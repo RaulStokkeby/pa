7826252bbd0e304c50c53846e8d3e70b9dcc9306
Raul Stokkeby <raul.stokkeby@khk.ee>
2019-09-25 11:06:48 +0300

    muutujate defineerimine ja kasutamine

---
+++ 25.09.2019/variables.php
@@ -0,0 +1,23 @@
+<?php
+// defineerin muutuja
+// $muutujaNimi = väärtus;
+
+// väärtused - andmed - andmetüübid
+// 3 tüüpi:
+//    tekst - string - "jutumärkide vahel"
+//    täisarvud - integer - 2,7,18,-9 jne
+//    komaga arvud e. reaalarvud - float - 3.5, 7.2094, -57,92 jne
+
+$eesNimi = "Raul";
+$pereNimi = "Stokkeby";
+$email = "raul.stokkeby@khk.ee";
+$vanus = 22;
+$pikkus = 1.77;
+$kaal = 70;
+
+echo "Eesnimi: " .$eesNimi. "<br>";
+echo "Perekonnanimi: " .$pereNimi. "<br>";
+echo "Email: " .$email. "<br>";
+echo "Vanus: " .$vanus. "<br>";
+echo "Pikkus: " .$pikkus. "<br>";
+echo "Kaal: " .$kaal. "<br>";
