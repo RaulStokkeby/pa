5a05c49e6c74ef96593cc7fc6170d744c8ae4542
Raul Stokkeby <raul.stokkeby@khk.ee>
2019-09-25 10:43:46 +0300

    Ülesanne 1

---
+++ 25.09.2019/ex_1.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * eesnimi:
+ * perekonnanimi:
+ * vanus:
+ * email:
+ * pikkus (m):
+ * kaal (kg):
+ */
+print "Eesnimi: Raul <br>";
+print "Perekonnanimi: Stokkeby <br>";
+print "Vanus: 22 <br>";
+print "Email: <a href='raul.stokkeby@khk.ee'>raul.stokkeby@khk.ee</a><br>";
+print "Pikkus (m): 1.77 <br>";
+print "Kaal (kg): 70";
