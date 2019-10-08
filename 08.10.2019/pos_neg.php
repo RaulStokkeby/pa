d543d3174d90773afa6f4626823b560cc6bdb027
Raul Stokkeby <raul.stokkeby@khk.ee>
2019-10-08 09:05:38 +0300

    if ja else lause

---
+++ 08.10.2019/pos_neg.php
@@ -0,0 +1,14 @@
+<?php
+
+$arv = -25;
+var_dump($arv > 0);
+
+if($arv >0) {
+    echo '<br>' . $arv . ' on positiivne!';
+} else {
+    echo '<br>' .$arv. ' on negatiivne!';
+    }
+
+
+echo '<br>programmi lõpp';
+?>
