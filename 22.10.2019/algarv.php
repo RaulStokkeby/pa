84681d2a10219414d36796041d9859e6e10e59ea
Raul Stokkeby <raul.stokkeby@khk.ee>
2019-10-22 10:28:07 +0300

    algarvu leidmine

---
+++ 22.10.2019/algarv.php
@@ -0,0 +1,22 @@
+<?php
+
+function algarv($number) {
+    if ($number < 2) {
+        echo $number.' - antud arv ei kuulu valitud vahemiku sisse <br>';
+    } else {
+        $divider = 2; // define divider
+        while ($number % $divider != 0) {
+            // while number is not divided by divider with modulo 0
+            $divider++; // get the next divider value
+        }
+        // if number and divider is equal - prime number
+        if ($number == $divider) {
+            echo $number . ' on algarv<br>';
+        } else {
+            // otherwise
+            echo $number . ' ei ole algarv<br>';
+        }
+    }
+}
+
+algarv(rand(0,1000));
