913eacca9ea3844f8fc5bd9e5b5dc5a0b1f2d40c
Raul Stokkeby <raul.stokkeby@khk.ee>
2019-10-22 10:31:21 +0300

    tagastamisega, result kasutamine

--- 22.10.2019/algarv.php
+++ 22.10.2019/algarv.php
@@ -2,21 +2,19 @@
 
 function algarv($number) {
     if ($number < 2) {
-        echo $number.' - antud arv ei kuulu valitud vahemiku sisse <br>';
+        $result =  $number.' - antud arv ei kuulu valitud vahemiku sisse <br>';
     } else {
-        $divider = 2; // define divider
+        $divider = 2;
         while ($number % $divider != 0) {
-            // while number is not divided by divider with modulo 0
-            $divider++; // get the next divider value
+            $divider++;
         }
-        // if number and divider is equal - prime number
         if ($number == $divider) {
-            echo $number . ' on algarv<br>';
+            $result =  $number . ' on algarv<br>';
         } else {
-            // otherwise
-            echo $number . ' ei ole algarv<br>';
+            $result =  $number . ' ei ole algarv<br>';
         }
     }
+    return $result;
 }
 
 algarv(rand(0,1000));
