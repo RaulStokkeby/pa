2399a5d5441d07aa0a09df3999018aae60e15e90
Raul Stokkeby <raul.stokkeby@khk.ee>
2019-10-22 10:50:56 +0300

    tagastamisega, result kasutamine, veel edasi värki

--- 22.10.2019/algarv.php
+++ 22.10.2019/algarv.php
@@ -2,19 +2,28 @@
 
 function algarv($number) {
     if ($number < 2) {
-        $result =  $number.' - antud arv ei kuulu valitud vahemiku sisse <br>';
+        echo  $number.' - antud arv ei kuulu valitud vahemiku sisse <br>';
     } else {
         $divider = 2;
         while ($number % $divider != 0) {
             $divider++;
         }
         if ($number == $divider) {
-            $result =  $number . ' on algarv<br>';
+            $result =  True;
         } else {
-            $result =  $number . ' ei ole algarv<br>';
+            $result =  False;
         }
     }
     return $result;
 }
 
-algarv(rand(0,1000));
+function tagastus($result) {
+    if ($result == True) {
+        echo 'on algarv';
+    } else {
+        echo 'ei ole algarv';
+    }
+
+}
+
+tagastus(algarv(rand(0,1000)));
